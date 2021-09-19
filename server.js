f0b212b12b6e1f32a13344170f3c2388241cd8f0
Hao Chen <hao.chen23@outlook.com>
2021-09-19 14:01:05 +1200

    Adds socket.io connection and http server

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require("express");
+const bodyParser = require("body-parser")
+
 const app = express();
+const http = require("http").Server(app)
+const io = require("socket.io")(http)
+
+
 
 app.use(express.static(__dirname))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
 
 let messages = [
     {name: "Tim", message: "Hi"},
@@ -13,10 +21,15 @@ app.get('/messages', (req, res) => {
 })
 
 app.post('/messages', (req, res) => {
-    console.log(req.body);
+    messages.push(req.body);
+    io.emit('message', req.body)
     res.sendStatus(200);
 })
-const server = app.listen(3000, () => {
+
+io.on('connection', (socket) => {
+    console.log("a user connected");
+})
+const server = http.listen(3000, () => {
     console.log(`Server listening on ${server.address().port}`)
 });
 
